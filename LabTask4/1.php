1eb8a6e14de4a06c3b456d433b4c73f8f4581bdd
Toufique1597 <77956303+Toufique1597@users.noreply.github.com>
2021-03-05 11:22:11 +0600

    Labtask 4

---
+++ LabTask4/1.php
@@ -0,0 +1,37 @@
+<html>
+<body>
+
+<table style="width:100%">
+	<tr>
+	<th>
+	<a href="http://localhost/lb4/1.php">Home</a>
+	<a href="http://localhost/lb4/login.php">Login</a>
+	<a href="http://localhost/lb4/REgistration%20form.php">Registration</a>
+	
+	<p><h1>Welcome to X company</h1></p></br>
+	</br>
+	</br>
+	</br>
+	</br>
+	</br>
+	</br>
+	</br>
+	</br>
+	<?php include 'footer 1.php';?>
+	
+	
+	</th>
+	</tr>
+	</b>
+<p><h1>X company</h1></p></br>
+
+
+  
+	
+
+
+
+
+
+</body>
+</html>
---
+++ LabTask4/footer2.php
@@ -0,0 +1,29 @@
+<html>
+<body>
+
+
+	<a href="http://localhost/lb4/1.php">Bob</a>
+	<a href="http://localhost/lb4/login.php">Logout</a>
+	
+
+	
+	
+	</br>
+	</br>
+	</br>
+	</br>
+	</br>
+	<h3> Account</h3>
+	
+	<ul>
+  <li> <a href="http://localhost/lb4/1.php">DashBoard</a>
+	</li>
+  <li><a href="http://localhost/lb4/1.php">View Profile</a></li>
+   <li><a href="http://localhost/lb4/1.php">Edit Profile</a></li>
+    <li><a href="http://localhost/lb4/1.php">Change Frofile Picture</a></li>
+	 <li><a href="http://localhost/lb4/1.php">Change Password</a></li>
+	  <li><a href="http://localhost/lb4/1.php">Log out</a></li>
+ 
+</ul>
+
+
